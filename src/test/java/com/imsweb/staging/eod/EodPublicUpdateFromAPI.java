e0e4f7ecbb02cd72ce4bb579b738abbcc93c6177
Chuck May <mayc@imsweb.com>
2020-11-05 08:10:52 -0500

    Trying to use the feature variants feature in Gradle.

--- src/test/java/com/imsweb/staging/eod/EodPublicUpdateFromAPI.java
+++ src/test/java/com/imsweb/staging/eod/EodPublicUpdateFromAPI.java
@@ -13,7 +13,7 @@ public class EodPublicUpdateFromAPI {
     private static final String _ALGORITHM = "eod_public";
 
     public static void main(String[] args) throws IOException {
-        UpdaterUtils.update(_ALGORITHM, EodVersion.v2_0.getVersion());
+        UpdaterUtils.update(_ALGORITHM, EodVersion.v2_0.getVersion(), "c:/tmp/algorithms");
     }
 
 }
