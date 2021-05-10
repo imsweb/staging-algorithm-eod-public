747de8eac67cab81a903e3adec54059644abc36b
Chuck May <mayc@imsweb.com>
2021-05-10 12:46:49 -0400

    Fixed unit test

--- src/test/java/com/imsweb/staging/StagingTest.java
+++ src/test/java/com/imsweb/staging/StagingTest.java
@@ -5,8 +5,8 @@ package com.imsweb.staging;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -19,17 +19,18 @@ import org.junit.Test;
 import com.imsweb.decisionengine.ColumnDefinition;
 import com.imsweb.decisionengine.Input;
 import com.imsweb.decisionengine.Mapping;
-import com.imsweb.decisionengine.Range;
+import com.imsweb.decisionengine.Output;
 import com.imsweb.decisionengine.Schema;
 import com.imsweb.decisionengine.Table;
-import com.imsweb.decisionengine.TableRow;
+import com.imsweb.staging.StagingData.Result;
+import com.imsweb.staging.eod.EodSchemaLookup;
+import com.imsweb.staging.eod.EodStagingData;
+import com.imsweb.staging.eod.EodStagingData.EodInput;
+import com.imsweb.staging.eod.EodStagingData.EodOutput;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 /**
  * Base class for all algorithm-specific testing
@@ -55,8 +56,8 @@ public abstract class StagingTest {
 
     @Test
     public void testInitialization() {
-        assertEquals(getAlgorithm(), _STAGING.getAlgorithm());
-        assertEquals(getVersion(), _STAGING.getVersion());
+        assertThat(_STAGING.getAlgorithm()).isEqualTo(getAlgorithm());
+        assertThat(_STAGING.getVersion()).isEqualTo(getVersion());
     }
 
     @Test
@@ -64,10 +65,10 @@ public abstract class StagingTest {
         for (String id : _STAGING.getTableIds()) {
             Table table = _STAGING.getTable(id);
 
-            assertNotNull(table);
-            assertNotNull(table.getAlgorithm());
-            assertNotNull(table.getVersion());
-            assertNotNull(table.getName());
+            assertThat(table).isNotNull();
+            assertThat(table.getAlgorithm()).isNotNull();
+            assertThat(table.getVersion()).isNotNull();
+            assertThat(table.getName()).isNotNull();
         }
     }
 
@@ -75,19 +76,19 @@ public abstract class StagingTest {
     public void testValidCode() {
         Map<String, String> context = new HashMap<>();
         context.put("hist", "7000");
-        assertFalse(_STAGING.isContextValid("prostate", "hist", context));
+        assertThat(_STAGING.isContextValid("prostate", "hist", context)).isFalse();
         context.put("hist", "8000");
-        assertTrue(_STAGING.isContextValid("prostate", "hist", context));
+        assertThat(_STAGING.isContextValid("prostate", "hist", context)).isTrue();
         context.put("hist", "8542");
-        assertTrue(_STAGING.isContextValid("prostate", "hist", context));
+        assertThat(_STAGING.isContextValid("prostate", "hist", context)).isTrue();
 
         // make sure null is handled
         context.put("hist", null);
-        assertFalse(_STAGING.isContextValid("prostate", "hist", context));
+        assertThat(_STAGING.isContextValid("prostate", "hist", context)).isFalse();
 
         // make sure blank is handled
         context.put("hist", "");
-        assertFalse(_STAGING.isContextValid("prostate", "hist", context));
+        assertThat(_STAGING.isContextValid("prostate", "hist", context)).isFalse();
     }
 
     @Test
@@ -96,59 +97,85 @@ public abstract class StagingTest {
         for (String id : _STAGING.getSchemaIds()) {
             Schema schema = _STAGING.getSchema(id);
             for (Input input : schema.getInputs()) {
-                assertNull("No schemas should have units", input.getUnit());
-                assertNull("No schemas should have decimal places", input.getDecimalPlaces());
+                assertThat(input.getUnit()).as("No schemas should have units").isNull();
+                assertThat(input.getDecimalPlaces()).as("No schemas should have decimal places").isNull();
             }
         }
     }
 
     @Test
     public void testValidSite() {
-        assertFalse(_STAGING.isValidSite(null));
-        assertFalse(_STAGING.isValidSite(""));
-        assertFalse(_STAGING.isValidSite("C21"));
-        assertFalse(_STAGING.isValidSite("C115"));
+        assertThat(_STAGING.isValidSite(null)).isFalse();
+        assertThat(_STAGING.isValidSite("")).isFalse();
+        assertThat(_STAGING.isValidSite("C21")).isFalse();
+        assertThat(_STAGING.isValidSite("C115")).isFalse();
 
-        assertTrue(_STAGING.isValidSite("C509"));
+        assertThat(_STAGING.isValidSite("C509")).isTrue();
     }
 
     @Test
     public void testValidHistology() {
-        assertFalse(_STAGING.isValidHistology(null));
-        assertFalse(_STAGING.isValidHistology(""));
-        assertFalse(_STAGING.isValidHistology("810"));
-        assertFalse(_STAGING.isValidHistology("8176"));
+        assertThat(_STAGING.isValidHistology(null)).isFalse();
+        assertThat(_STAGING.isValidHistology("")).isFalse();
+        assertThat(_STAGING.isValidHistology("810")).isFalse();
+        assertThat(_STAGING.isValidHistology("8176")).isFalse();
 
-        assertTrue(_STAGING.isValidHistology("8000"));
-        assertTrue(_STAGING.isValidHistology("8201"));
+        assertThat(_STAGING.isValidHistology("8000")).isTrue();
+        assertThat(_STAGING.isValidHistology("8201")).isTrue();
+    }
+
+    @Test
+    public void testAllowedFields() {
+        Set<String> descriminators = new HashSet<>();
+        descriminators.add(EodStagingData.PRIMARY_SITE_KEY);
+        descriminators.add(EodStagingData.HISTOLOGY_KEY);
+
+        for (String id : _STAGING.getSchemaIds()) {
+            Schema schema = _STAGING.getSchema(id);
+            if (schema.getSchemaDiscriminators() != null)
+                descriminators.addAll(schema.getSchemaDiscriminators());
+        }
+
+        assertThat(descriminators).containsExactlyInAnyOrder(
+                EodStagingData.PRIMARY_SITE_KEY,
+                EodStagingData.HISTOLOGY_KEY,
+                EodInput.SEX.toString(),
+                EodInput.BEHAVIOR.toString(),
+                EodInput.DX_YEAR.toString(),
+                EodInput.DISCRIMINATOR_1.toString(),
+                EodInput.DISCRIMINATOR_2.toString());
     }
 
     @Test
     public void testGetTable() {
-        assertNull(_STAGING.getTable("bad_table_name"));
+        assertThat(_STAGING.getTable("bad_table_name")).isNull();
     }
 
     @Test
     public void testCachedSiteAndHistology() {
         StagingDataProvider provider = getProvider();
-        assertTrue(provider.getValidSites().size() > 0);
-        assertTrue(provider.getValidHistologies().size() > 0);
+        assertThat(provider.getValidSites().size() > 0).isTrue();
+        assertThat(provider.getValidHistologies().size() > 0).isTrue();
 
         // site tests
         List<String> validSites = Arrays.asList("C000", "C809");
         List<String> invalidSites = Arrays.asList("C727", "C810");
         for (String site : validSites)
-            assertTrue(provider.getValidSites().contains(site));
+            assertThat(provider.getValidSites().contains(site)).isTrue();
         for (String site : invalidSites)
-            assertFalse(provider.getValidSites().contains(site));
+            assertThat(provider.getValidSites().contains(site)).isFalse();
 
         // hist tests
-        List<String> validHist = Arrays.asList("8000", "8002", "8005", "8290", "9992");
-        List<String> invalidHist = Arrays.asList("8006", "9993");
+        List<String> validHist = Arrays.asList("8000", "8002", "8005", "8290", "9992", "9993");
+        List<String> invalidHist = Arrays.asList("8006", "8444");
         for (String hist : validHist)
-            assertTrue(provider.getValidHistologies().contains(hist));
+            assertThat(provider.getValidHistologies().contains(hist))
+                    .withFailMessage("The histology '" + hist + "' is not in the valid histology list")
+                    .isTrue();
         for (String hist : invalidHist)
-            assertFalse(provider.getValidHistologies().contains(hist));
+            assertThat(provider.getValidHistologies().contains(hist))
+                    .withFailMessage("The histology '" + hist + "' is not supposed to be in the valid histology list")
+                    .isFalse();
     }
 
     @Test
@@ -181,7 +208,7 @@ public abstract class StagingTest {
 
                     // make sure the input key matches the an input column
                     if (!inputKeys.contains(input.getKey()))
-                        errors.add("Input key " + schemaId + ":" + input.getKey() + " does not match validation table " + table.getId() + ": " + inputKeys.toString());
+                        errors.add("Input key " + schemaId + ":" + input.getKey() + " does not match validation table " + table.getId() + ": " + inputKeys);
                 }
             }
         }
@@ -226,6 +253,118 @@ public abstract class StagingTest {
         assertNoErrors(errors, "input values and their assocated validation tables");
     }
 
+    @Test
+    public void testBehaviorDescriminator() {
+        // test valid combination that requires discriminator and a good discriminator is supplied
+        EodSchemaLookup lookup = new EodSchemaLookup("C717", "9591");
+        List<Schema> lookups = _STAGING.lookupSchema(lookup);
+        assertThat(lookups).hasSize(3);
+        lookup.setInput(EodInput.DISCRIMINATOR_1.toString(), "1");
+        lookup.setInput(EodInput.BEHAVIOR.toString(), "3");
+        lookups = _STAGING.lookupSchema(lookup);
+        assertThat(lookups).hasSize(1);
+        assertThat(lookups.get(0).getId()).isEqualTo("hemeretic");
+    }
+
+    @Test
+    public void testStagingEnums() {
+        Set<String> enumInput = Arrays.stream(EodInput.values()).map(EodInput::toString).collect(Collectors.toSet());
+        Set<String> enumOutput = Arrays.stream(EodOutput.values()).map(EodOutput::toString).collect(Collectors.toSet());
+
+        // collect all input and output fields from all schemas
+        Set<String> schemaInput = new HashSet<>();
+        Set<String> schemaOutput = new HashSet<>();
+        for (String schemaId : _STAGING.getSchemaIds()) {
+            Schema schema = _STAGING.getSchema(schemaId);
+
+            schemaInput.addAll(_STAGING.getInputs(schema));
+            schemaOutput.addAll(_STAGING.getOutputs(schema));
+        }
+
+        assertThat(schemaInput).hasSameElementsAs(enumInput);
+        assertThat(schemaOutput).hasSameElementsAs(enumOutput);
+    }
+
+    @Test
+    public void testNaaccrXmlIds() {
+        List<String> errors = new ArrayList<>();
+
+        Map<String, Set<String>> inputMappings = new HashMap<>();
+        Map<String, Set<String>> outputMappings = new HashMap<>();
+        for (String schemaId : _STAGING.getSchemaIds()) {
+            Schema schema = _STAGING.getSchema(schemaId);
+
+            for (Input input : schema.getInputs()) {
+                if (input.getNaaccrItem() != null && input.getNaaccrXmlId() == null)
+                    errors.add("Schema input " + schema.getId() + "." + input.getKey() + " has a NAACCR number but is missing NAACCR XML ID");
+
+                if (input.getNaaccrXmlId() != null) {
+                    if (inputMappings.containsKey(input.getNaaccrXmlId()))
+                        inputMappings.get(input.getNaaccrXmlId()).add(input.getKey());
+                    else
+                        inputMappings.put(input.getNaaccrXmlId(), new HashSet<>(Collections.singletonList(input.getKey())));
+                }
+            }
+
+            for (Output output : schema.getOutputs()) {
+                if (output.getNaaccrItem() != null && output.getNaaccrXmlId() == null)
+                    errors.add("Schema output " + schema.getId() + "." + output.getKey() + " has a NAACCR number but is missing NAACCR XML ID");
+
+                if (output.getNaaccrXmlId() != null) {
+                    if (outputMappings.containsKey(output.getNaaccrXmlId()))
+                        outputMappings.get(output.getNaaccrXmlId()).add(output.getKey());
+                    else
+                        outputMappings.put(output.getNaaccrXmlId(), new HashSet<>(Collections.singletonList(output.getKey())));
+                }
+            }
+        }
+
+        // verify that if a field has a given NAACCR XML ID, then all fields with that same XML ID have the same key.
+        inputMappings.forEach((k, v) -> {
+            if (v.size() > 1)
+                errors.add("NAACCR XML Id " + k + " is listed for multiple inputs: " + v);
+        });
+        outputMappings.forEach((k, v) -> {
+            if (v.size() > 1)
+                errors.add("NAACCR XML Id " + k + " is listed for multiple outputs: " + v);
+        });
+
+        assertThat(errors).overridingErrorMessage(() -> "\n" + String.join("\n", errors)).isEmpty();
+    }
+
+    @Test
+    public void testMisspelledProperty() {
+        EodStagingData data = new EodStagingData();
+        data.setInput(EodInput.DX_YEAR, "2020");
+        data.setInput(EodInput.PRIMARY_SITE, "C180");
+        data.setInput(EodInput.HISTOLOGY, "8000");
+        data.setInput(EodInput.NODES_POS, "90");
+        data.setInput(EodInput.EOD_PRIMARY_TUMOR, "700");
+        data.setInput(EodInput.EOD_REGIONAL_NODES, "300");
+        data.setInput(EodInput.EOD_METS, "10");
+
+        // perform the staging
+        _STAGING.stage(data);
+
+        assertEquals(Result.STAGED, data.getResult());
+        assertEquals("colon_rectum", data.getSchemaId());
+        assertEquals(0, data.getErrors().size());
+        assertEquals(11, data.getPath().size());
+
+        // before the bug fix, AJCC_VERSION_NUMBER was returning an empty string
+        assertEquals("08", data.getOutput(EodOutput.AJCC_VERSION_NUMBER));
+
+        // check other output
+        assertEquals("00200", data.getOutput(EodOutput.NAACCR_SCHEMA_ID));
+        assertEquals("4A", data.getOutput(EodOutput.EOD_2018_STAGE_GROUP));
+        assertEquals("2.0", data.getOutput(EodOutput.DERIVED_VERSION));
+        assertEquals("7", data.getOutput(EodOutput.SS_2018_DERIVED));
+        assertEquals("T4b", data.getOutput(EodOutput.EOD_2018_T));
+        assertEquals("N2b", data.getOutput(EodOutput.EOD_2018_N));
+        assertEquals("M1a", data.getOutput(EodOutput.EOD_2018_M));
+        assertEquals("20", data.getOutput(EodOutput.AJCC_ID));
+    }
+
     /**
      * Helper method to assert failures when tracked errors exist
      */
@@ -233,43 +372,8 @@ public abstract class StagingTest {
         if (!errors.isEmpty()) {
             System.out.println("There were " + errors.size() + " issues with " + description + ".");
             errors.forEach(System.out::println);
-            fail();
+            fail("There were " + errors.size() + " issues with " + description + ".");
         }
     }
 
-    /**
-     * Return the input length from a specified table
-     * @param tableId table indentifier
-     * @param key input key
-     * @return null if no length couild be determined, or the length
-     */
-    protected Integer getInputLength(String tableId, String key) {
-        Table table = _STAGING.getTable(tableId);
-        Integer length = null;
-
-        // loop over each row
-        for (TableRow row : table.getTableRows()) {
-            List<Range> ranges = row.getInputs().get(key);
-
-            for (Range range : ranges) {
-                String low = range.getLow();
-                String high = range.getHigh();
-
-                if (range.matchesAll() || low.isEmpty())
-                    continue;
-
-                if (low.startsWith("{{") && low.contains(Staging.CTX_YEAR_CURRENT))
-                    low = String.valueOf(Calendar.getInstance().get(Calendar.YEAR));
-                if (high.startsWith("{{") && high.contains(Staging.CTX_YEAR_CURRENT))
-                    high = String.valueOf(Calendar.getInstance().get(Calendar.YEAR));
-
-                if (length != null && (low.length() != length || high.length() != length))
-                    throw new IllegalStateException("Inconsistent lengths in table " + tableId + " for key " + key);
-
-                length = low.length();
-            }
-        }
-
-        return length;
-    }
 }
